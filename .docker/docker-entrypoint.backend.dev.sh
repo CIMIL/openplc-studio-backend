51fdf1719171921079c617067d06780584970bfe
filippodaniotti <febbrosto@gmail.com>
2026-09-19 13:02:52 +0200

    migrate to uv

--- .docker/docker-entrypoint.backend.dev.sh
+++ .docker/docker-entrypoint.backend.dev.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-poetry run python3 -m debugpy --listen 0.0.0.0:5678 plc_platform_backend/debug.py
+uv run --no-sync python3 -m debugpy --listen 0.0.0.0:5678 plc_platform_backend/debug.py
--- .docker/docker-entrypoint.workers.sh
+++ .docker/docker-entrypoint.workers.sh
@@ -2,7 +2,7 @@
 
 delay=1
 while true; do
-  poetry run dramatiq plc_platform_backend.actors
+  uv run --no-sync dramatiq plc_platform_backend.actors
   if [ $? -eq 3 ]; then
     echo "Connection error encountered on startup. Retrying in $delay second(s)..."
     sleep $delay
